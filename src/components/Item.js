d42243314cb58b94f54ea3e22983627bee1b9b47
Ian Kimani <ianmwng@gmail.com>
2023-10-30 15:26:56 +0300

    complete assinngment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,43 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  function toggleCartItem() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then(r => r.json())
+      .then(updatedItem => onUpdateItem(updatedItem));
+  }
+
+  function handleDeleteClick() {
+    // Delete the item from the API
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+      .then(r => r.json())
+      .then(() => onDeleteItem(item));
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button
+        onClick={toggleCartItem}
+        className={item.isInCart ? "remove" : "add"}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button onClick={handleDeleteClick} className="remove">
+        Delete
+      </button>
     </li>
   );
 }
